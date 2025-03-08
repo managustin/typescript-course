c325bfbb64950803d5221028754bd60547745147
Robertunix <managustin01@gmail.com>
2025-03-08 22:54:58 +0000

    Any, Unknown, Castings, Optional Chaining and Bang

--- app.js
+++ app.js
@@ -1,16 +1,4 @@
 "use strict";
-var arr0 = [1, 2, 3];
-console.log(arr0);
-var arr1 = [["hello0", 1], ["hello1"], ["hello2"]];
-// coordinate pair x,y
-// a tuple is a fixed length array that has defined values for each position in the array
-const coord = [1, '2'];
-coord[0] = 2; // ok
-//coord[1] = 2;   // not ok
-console.log(coord[0]);
-const coords1 = [
-    [1, 2],
-    [-1, 3],
-    [3, 4]
-];
-console.log(`En coords1[0][1] hay ${coords1[2][1]}`);
+let x = 1;
+x();
+console.log(x.length);
--- app.ts
+++ app.ts
@@ -1,11 +1,28 @@
-// Literal is a textual representation (notation) of a value as it is written in source code. E.g.: 23, 'hello', True.
+let x: any = 1; // 'any' allows to do some weird things with the variable, as follows
+x();            // this is probably not gonna compile tho.
+console.log(x.length);
 
-let direction: "north" | "south" | "east" | "west"; // these are the values that can be assigned to the direction variable;
+let y: unknown = 1; // unknown type provides a way to handle values of uncertain types while maintaining type safety.
 
-direction = 'south';
-// direction = 'hello'; //doesn't work
+if (typeof y == 'number') { // here it was asserted that the type of y is number 
+    const result = y + 1;
+} else if (typeof y == "string") {
+    const result = y.length
+}
 
-let responseCode: 200 | 404 | 201;
-responseCode = 200;
-let responseCode1: true;
-responseCode1 = false; 
+const result = (y as number) + 1 // type cast
+
+
+
+/*Optional Chaining and Bang**/
+
+const arr = [{name: 'robert'}, {name: 'joe'}, {name: 'ana'}]
+
+const el0 = arr.pop()?.name // since the pop method could return 'undefined' (which would throw an error), a question mark is placed.
+                            // the ? checks if the result from the statement is undefined or not. If it is, it will assign 'undefined' 
+                            // to the variable el. If it's not, it will continue through the chain and look at the next property, i.e., name
+
+const nestedArray = [[{name: "Juan"}]]
+const el1 = nestedArray.pop()?.pop()?.name  // another example
+
+const el2 = nestedArray.pop()!.pop()!.name  // the exclamation point operator tells the compiler to ignore the possibility of it being undefined.
