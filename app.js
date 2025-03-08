b6c474e6bdd0245b5048976808f1b6423e421024
Robertunix <managustin01@gmail.com>
2025-03-08 18:49:57 +0000

    Configured project setup and dependencies

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
+"use strict";
 let x = 2; //this implicitly sets the type of the variable to number
-
-let y: number = 2; //this explicitly sets the type.
+let y = 2; //this explicitly sets the type.
