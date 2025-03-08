117b08def2010ef0baae9616d984a0a6bc66084f
Robertunix <managustin01@gmail.com>
2025-03-08 20:14:13 +0000

    Primitive types

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 "use strict";
 let x = 2; //this implicitly sets the type of the variable to number
 let y = 2; //this explicitly sets the type.
+let z;
+z = Infinity;
+z = 67.2;
+let cadena;
+cadena = `The z variable is equal to ${z}`;
+console.log(cadena);
--- app.ts
+++ app.ts
@@ -1,3 +1,19 @@
 let x = 2; //this implicitly sets the type of the variable to number
 
 let y: number = 2; //this explicitly sets the type.
+
+let z: number;
+z = Infinity;
+z = 67.2;
+
+let cadena: string;
+cadena = `The z variable is equal to ${z}`;
+console.log(cadena);
+
+let result0 = null; //null define explicitly something as empty or non-existent. by default the type is 'any'
+let result1 = undefined; //is more used as a placeholder that means a variable has been declared but has not yet been assigned a value
+result0 = 2;
+result1= 'hello'
+
+let result2: number | undefined = undefined;   //result2 can have either of the two types separated with the union.
+result2 = 2;
