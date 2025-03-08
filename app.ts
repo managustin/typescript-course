5012bc1029262375787ff6aa32c843a9f508681f
Robertunix <managustin01@gmail.com>
2025-03-08 21:44:17 +0000

    Enums & Literals

--- app.ts
+++ app.ts
@@ -1,19 +1,11 @@
-var arr0: number[] = [1, 2, 3];
-console.log(arr0);
+// Literal is a textual representation (notation) of a value as it is written in source code. E.g.: 23, 'hello', True.
 
-var arr1 = [["hello0", 1], ["hello1"], ["hello2"]];
+let direction: "north" | "south" | "east" | "west"; // these are the values that can be assigned to the direction variable;
 
-// coordinate pair x,y
-// a tuple is a fixed length array that has defined values for each position in the array
-const coord: [number, string] = [1,'2'];
-coord[0] = 2;   // ok
-//coord[1] = 2;   // not ok
-console.log(coord[0]);
+direction = 'south';
+// direction = 'hello'; //doesn't work
 
-const coords1: [number, number][] = [ // this is an array of tuples
-    [1,2],
-    [-1,3],
-    [3,4]
-]
-
-console.log(`En coords1[0][1] hay ${coords1[2][1]}`);
+let responseCode: 200 | 404 | 201;
+responseCode = 200;
+let responseCode1: true;
+responseCode1 = false; 
